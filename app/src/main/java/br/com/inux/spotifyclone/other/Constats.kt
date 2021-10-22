6d4f873048668bdb18b3eea4fea40b957cb5dc17
victor <victor.alexandre@atlantasistemas.com>
2021-10-21 21:36:57 -0300

    Music notification

--- app/src/main/java/br/com/inux/spotifyclone/other/Constats.kt
+++ app/src/main/java/br/com/inux/spotifyclone/other/Constats.kt
@@ -2,4 +2,6 @@ package br.com.inux.spotifyclone.other
 
 object Constats {
     const val SONG_COLLECTION = "songs"
+    const val NOTIFICATION_CHANNEL_ID = "music"
+    const val NOTIFICATION_ID = 1
 }
